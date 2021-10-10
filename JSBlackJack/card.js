f9b1a38be41f764bf04bd6f8737a757d79981bc1
Jason Tsorlinis <jtsorlinis@gmail.com>
2021-10-11 09:36:50 +1100

    tiny cleanup
    Former-commit-id: e49ba279d76134c0283d55b67aa7cf37ff081acd

--- JSBlackJack/card.js
+++ JSBlackJack/card.js
@@ -1,39 +1,43 @@
 module.exports = class Card {
-  constructor (rank, suit) {
-    this.mRank = rank
-    this.mSuit = suit
-    this.mFaceDown = false
-    this.mValue = this.evaluate()
-    this.mCount = this.count()
-    this.mIsAce = false
-    if (this.mRank === 'A') {
-      this.mIsAce = true
-    }
+  constructor(rank, suit) {
+    this.mRank = rank;
+    this.mSuit = suit;
+    this.mFaceDown = false;
+    this.mValue = this.evaluate();
+    this.mCount = this.count();
+    this.mIsAce = this.mRank === "A";
   }
 
-  print () {
+  print() {
     if (this.mFaceDown) {
-      return 'X'
+      return "X";
     }
-    return this.mRank
+    return this.mRank;
   }
 
-  evaluate () {
-    if (this.mRank === 'J' || this.mRank === 'Q' || this.mRank === 'K') {
-      return 10
-    } if (this.mRank === 'A') {
-      return 11
+  evaluate() {
+    if (this.mRank === "J" || this.mRank === "Q" || this.mRank === "K") {
+      return 10;
     }
-    return parseInt(this.mRank, 10)
+    if (this.mRank === "A") {
+      return 11;
+    }
+    return parseInt(this.mRank, 10);
   }
 
-  count () {
-    if (this.mRank === '10' || this.mRank === 'J' ||
-        this.mRank === 'Q' || this.mRank === 'K' || this.mRank === 'A') {
-      return -1
-    } if (this.mRank === '7' || this.mRank === '8' || this.mRank === '9') {
-      return 0
+  count() {
+    if (
+      this.mRank === "10" ||
+      this.mRank === "J" ||
+      this.mRank === "Q" ||
+      this.mRank === "K" ||
+      this.mRank === "A"
+    ) {
+      return -1;
     }
-    return 1
+    if (this.mRank === "7" || this.mRank === "8" || this.mRank === "9") {
+      return 0;
+    }
+    return 1;
   }
-}
+};
