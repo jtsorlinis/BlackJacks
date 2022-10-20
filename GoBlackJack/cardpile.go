1f48e20ba0304f5c4d6cd1e4a8b42a800ac21df3
Jason Tsorlinis <jtsorlinis@gmail.com>
2022-10-20 23:22:29 +1100

    Fixed go wyrand to match rust and cpp
    Former-commit-id: a2446586d29452eaf3d28969e3aa8727a68b60f1

--- GoBlackJack/cardpile.go
+++ GoBlackJack/cardpile.go
@@ -8,26 +8,26 @@ import (
 var state = uint64(time.Now().Unix())
 
 // From https://github.com/lemire/testingRNG
-func wyrand() uint32 {
+func wyrand() uint64 {
 	state += 0xa0761d6478bd642f
 	hi, lo := bits.Mul64(state^0xe7037ed1a0b428db, state)
-	return uint32(hi ^ lo)
+	return uint64(hi ^ lo)
 }
 
 // use nearly divisionless technique found here https://github.com/lemire/FastShuffleExperiments
-func rand_range(s uint32) uint32 {
+func rand_range(s uint64) uint64 {
 	x := wyrand()
-	m := uint64(x) * uint64(s)
-	l := uint32(m)
+	mhi, mlo := bits.Mul64(x, s)
+	l := mlo
 	if l < s {
 		t := -s % s
 		for l < t {
 			x = wyrand()
-			m = uint64(x) * uint64(s)
-			l = uint32(m)
+			mhi, mlo = bits.Mul64(x, s)
+			l = mlo
 		}
 	}
-	return uint32(m >> 32)
+	return mhi
 }
 
 // CardPile class
@@ -59,7 +59,7 @@ func (c *CardPile) Print() string {
 
 // Shuffle the cards
 func (c *CardPile) Shuffle() {
-	var i = uint32(len(c.MCards) - 1)
+	var i = uint64(len(c.MCards) - 1)
 	for ; i > 0; i-- {
 		j := rand_range(i + 1)
 		c.MCards[i], c.MCards[j] = c.MCards[j], c.MCards[i]
