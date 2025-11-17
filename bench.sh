94b899f2db117d30b43bac1f9743b1c959b0b16c
Jason <jtsorlinis@gmail.com>
2025-11-17 16:02:25 +1100

    version bumps

--- bench.sh
+++ bench.sh
@@ -21,6 +21,7 @@ cd ../..
 echo "Building CSharpBlackJack"
 cd CSharpBlackJack
 dotnet build -c release
+dotnet publish
 cd ..
 
 echo "Building GoBlackJack"
@@ -37,9 +38,11 @@ hyperfine --warmup 3 -L num_rounds $ROUNDS --export-markdown results.md --sort m
 -n C "CBlackJack/build/bin/CBlackJack {num_rounds}" \
 -n C++ "CPPBlackJack/build/bin/CPPBlackJack {num_rounds}" \
 -n Rust "RustBlackJack/target/release/rust_black_jack {num_rounds}" \
--n C# "dotnet CSharpBlackJack/bin/Release/net9.0/CSharpBlackJack.dll {num_rounds}" \
+-n C# "dotnet CSharpBlackJack/bin/Release/net10.0/CSharpBlackJack.dll {num_rounds}" \
+-n "C# (AOT)" "CSharpBlackJack/bin/Release/net10.0/osx-arm64/native/CSharpBlackJack {num_rounds}" \
 -n Go "GoBlackJack/GoBlackJack {num_rounds}" \
 -n Node "node JSBlackJack/. {num_rounds}" \
 -n Bun "bun JSBlackJack/main.js {num_rounds}" \
--n Python "python3 PyBlackJack/main.py {num_rounds}" 
+-n PyPy "pypy3 PyBlackJack/main.py {num_rounds}" \
+-n Python "python3 PyBlackJack/main.py {num_rounds}"
 exit 0
